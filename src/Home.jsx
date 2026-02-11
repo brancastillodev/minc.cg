ebd0988ee0a2c90cf2408ab057031d4fcf81565c
Brandon Castillo <brandoncastillo.09@gmail.com>
2026-02-11 18:29:28 -0300

    testing preload auto offf

--- src/Home.jsx
+++ src/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
         {isMobile 
         ?
         <div className="home-animated-bar">
-          <video loop muted autoPlay playsInline preload="auto">
+          <video loop muted autoPlay playsInline>
             <source src={barMob} type="video/quicktime"/>
             Your browser doesn't support animations :,
           </video>
