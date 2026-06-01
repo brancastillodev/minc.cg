b5f28f04f3d84e8edce9eb0ef12556cb8c6d842e
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-06-01 18:29:47 -0300

    data item url absoluta

--- src/commons/AddCartButton.jsx
+++ src/commons/AddCartButton.jsx
@@ -12,7 +12,7 @@ function AddCartButton({product}){
       data-item-description={`${product.size} | Code: ${product.sku}`}
       data-item-quantity={1}
       data-item-max-quantity="1"
-      data-item-url={`/market/${product.id}`}
+      data-item-url={`https://minc-cg.com/market/${product.id}`}
     >             
       <img src={addToCart} alt={"add to cart button"}/>
     </figure>
