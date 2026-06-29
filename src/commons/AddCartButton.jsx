5b1ec4604fb95cdda5f3939ba7d769b81f36e47c
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-06-28 23:00:53 -0300

    new data item url with single product page url

--- src/commons/AddCartButton.jsx
+++ src/commons/AddCartButton.jsx
@@ -12,7 +12,7 @@ function AddCartButton({product}){
       data-item-description={`${product.size} | Code: ${product.sku}`}
       data-item-quantity={1}
       data-item-max-quantity="1"
-      data-item-url={`https://www.minc-cg.com/market`}
+      data-item-url={`https://www.minc-cg.com/market/${product.id}`}
     >             
       <img src={addToCart} alt={"add to cart button"}/>
     </figure>
