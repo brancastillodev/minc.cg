4e723f7039ef23b461de2c3e5f127dcb54b9cb4a
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-08-17 19:58:52 -0300

    sass pipeline with hmr, centered home layout and product page fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './styles/app.css'
+import './styles/app.sass'
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './componets/Navbar';
 import Footer from './componets/Footer';
